5a916a0c327d354fddcd293d6c421e007d1b4b35
yanming <ymbusy@126.com>
2018-11-16 03:11:29 -0800

    first test

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<<"this is the first program"<< std::endl;
+    std::cout<<"this is the first program"<< std::endl;
+    return 0;
+}
